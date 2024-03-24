584473f0a639bbea991cfd9710194e2346c8dbd2
Tanvir Ramim <ramimhossan14@gmail.com>
2024-03-24 16:50:35 +0600

    try to added filter ui

--- src/Pages/TaskBoard/TaskBoard.jsx
+++ src/Pages/TaskBoard/TaskBoard.jsx
@@ -1,11 +1,35 @@
+
 import TopPart from "./TopPart";
 
 
 const TaskBoard = () => {
+    
     return (
         <div className="bg-gradient-to-r from-pink-200 to-blue-300 min-h-screen">
             <div className="max-w-[1400px] pt-16 m-auto">
                 <TopPart></TopPart>
+                <div className="border">
+                    <div>
+                        <div>
+                        <div>
+                           <span>Filter : </span>
+                           <input placeholder ='Assignee Name'className="rounded p-1 " type="text" name="name" />
+                           
+                           <select className="py-1  rounded ml-5"  name="priority" id="">
+                            <option  selected hidden disabled  >Priority</option>
+                            <option value="">P0</option>
+                            <option value="">P1</option>
+                            <option value="">P2</option>
+                           </select>
+
+
+                        </div>
+                        </div>
+                        <div>
+                            <button className="bg-blue-600 text-white px-5 py-2 rounded">Add New Task </button>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
