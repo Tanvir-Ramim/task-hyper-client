deab4b071820752ddd77be5ca732711057b62c00
Tanvir Ramim <ramimhossan14@gmail.com>
2024-03-29 16:13:20 +0600

    home button added in taskboard

--- src/Pages/TaskBoard/topPart/TopPart.jsx
+++ src/Pages/TaskBoard/topPart/TopPart.jsx
@@ -2,12 +2,18 @@ import { useContext } from "react";
 
 import pp from '../../../assets/no-pp.jpg'
 import { AuthContext } from "../../../Provider/AuthProvider";
+import { Link } from "react-router-dom";
+
 
 const TopPart = () => {
     const {user}=useContext(AuthContext)
     return (
-        <div className="flex justify-between items-center p-2">
+        <div className="flex justify-between flex-wrap items-center gap-1 p-2">
            <div> <h1 className="text-3xl font-bold">Task Board</h1></div>
+             <div>
+               
+               <Link to={'/'} className="text-3xl font-bold underline">Home</Link>
+             </div>
             <div className="flex items-center border-2 border-pink-200 p-2">
             <div className="avatar">
                         <div className="w-9 mr-2 rounded-full ring-primary ring-offset-base-100 ring-offset-2">
@@ -23,6 +29,7 @@ const TopPart = () => {
                 </div>
                 
             </div>
+
         </div>
     );
 };
--- src/hooks/useAxios.jsx
+++ src/hooks/useAxios.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const myAxios=axios.create({
- baseURL: 'http://localhost:5000'
+ baseURL: 'https://task-hyper-server.vercel.app'
 })
   const useAxios=()=>{
       return myAxios
